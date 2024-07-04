dc96efbe4c7d72ea46d418b3bd9b4bb7b191de37
MilindAdiga <124535083+MilindAdiga@users.noreply.github.com>
2024-07-04 20:19:02 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -8,6 +8,22 @@ function add(numbers) {
    if (numbers === "0") {
         return 0;
     }
+
+  // Handle two numbers
+  const numArray = numbers.split(",");
+    if (numArray.length === 2) {
+        return parseInt(numArray[0]) + parseInt(numArray[1]);
+    }
+
+  // Ignore Numbers Greater Than 1000
+  let sum = 0;
+    for (let num of numArray) {
+        const parsedNum = parseInt(num);
+        if (parsedNum <= 1000) {
+            sum += parsedNum;
+        }
+    }
+    return sum;
 }
 
 module.exports = {
